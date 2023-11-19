fe6f567499a2e0c6d04d7c105628b87fb70bb85a
Andrew Simmons <agsimmons0@gmail.com>
2023-11-18 22:39:42 -0500

    Refactor config.h

--- keyboards/boardsource/lulu/keymaps/agsimmons0/config.h
+++ keyboards/boardsource/lulu/keymaps/agsimmons0/config.h
@@ -1,6 +1,7 @@
 #pragma once
 
-// Disable default RGB Matrix animations
+// ===== Resets ===============================================================
+// ----- RGB Matrix -----------------------------------------------------------
 #undef ENABLE_RGB_MATRIX_ALPHAS_MODS
 #undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
 #undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
@@ -8,10 +9,13 @@
 #undef ENABLE_RGB_MATRIX_BAND_SAT
 #undef ENABLE_RGB_MATRIX_BAND_VAL
 
+// ===== Customizations =======================================================
+// ----- RGB Matrix -----------------------------------------------------------
 // https://docs.qmk.fm/#/feature_split_keyboard?id=data-sync-options
 // Used for RGB Matrix Multisplash Effect
 #define SPLIT_TRANSPORT_MIRROR
 
 #define ENABLE_RGB_MATRIX_MULTISPLASH
 
+// ----- Misc -----------------------------------------------------------------
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
